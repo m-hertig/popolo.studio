c01c6a8593041023dd4e7246cd9bec51906654e2
Martin Hertig <martin.hertig@gmail.com>
2019-04-11 16:39:28 +0200

    update styles

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -7,9 +7,17 @@ var intervalID;
 // To avoid flash of unstyled content
 $('html').addClass('hidden');
 $(window).on('load', function () {
+  shuffleElements($('.team li') );
   $('html').removeClass('hidden');
  });
 
+ $(".cta_button").click(function(){
+   $(".contact-form").slideDown("fast");
+ });
+
+ $(".close").click(function(){
+   $(".contact-form").slideUp("fast");
+ });
 
 function nextText(){
     $('.popolo-variable').fadeOut(300, function () {
@@ -60,3 +68,35 @@ $(".button_education").hover( function () {
   $(this).fadeIn(100);
   window.clearInterval(intervalID);
 });
+
+
+
+function shuffleElements($elements) {
+	var i, index1, index2, temp_val;
+
+	var count = $elements.length;
+	var $parent = $elements.parent();
+	var shuffled_array = [];
+
+
+	// populate array of indexes
+	for (i = 0; i < count; i++) {
+		shuffled_array.push(i);
+	}
+
+	// shuffle indexes
+	for (i = 0; i < count; i++) {
+		index1 = (Math.random() * count) | 0;
+		index2 = (Math.random() * count) | 0;
+
+		temp_val = shuffled_array[index1];
+		shuffled_array[index1] = shuffled_array[index2];
+		shuffled_array[index2] = temp_val;
+	}
+
+	// apply random order to elements
+	$elements.detach();
+	for (i = 0; i < count; i++) {
+		$parent.append( $elements.eq(shuffled_array[i]) );
+	}
+}
